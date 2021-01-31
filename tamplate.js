9189f0b51b7ffe17b26e3cb68736f702b581c84a
shakil51298 <zikaiwen02@gmail.com>
2021-02-01 04:09:45 +0800

    ss

--- tamplate.js
+++ tamplate.js
@@ -1,5 +1,12 @@
+//template string , multiline string
+
 const FirstName = "justin";
 const LastName = "TimverLake"
-const Total = `${FirstName} ${LastName} is a good boy`;//template 
+const Total = `${FirstName} ${LastName} is a good boy`;//template ফাকা বানাইছে!!!
 // const Total = FirstName +' '+ LastName;
-console.log(Total);
+//multilie with Es6;
+// const shakil = 'love you \n'+'miss you'//old system
+const multilie2 = `i love you,
+i miss you, 
+i waaaaa fu*k you!`
+console.log(multilie2);
